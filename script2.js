6c6408976bf8c6da2071de57c3071105f5719f6b
Mitchell Barclay <mitchellbarclay2@gmail.com>
2019-05-27 19:09:54 +1200

    commit

--- script2.js
+++ script2.js
@@ -8,9 +8,52 @@ var random_images_array = ["img/static1.png", "img/static2.png", "img/static3.pn
 
 
 function newSrc() {
-      var e = document.getElementById("SelectMenu");
-      var newSrc = e.options[e.selectedIndex].value;
-      document.getElementById("MyFrame").src=newSrc;
+
+
+      // var e = document.getElementById("SelectMenu");
+      // var newSrc = e.options[e.selectedIndex].value;
+      // document.getElementById("MyFrame").src=newSrc;
+
+       var f = document.getElementById("MyFrame");
+       var d = document.getElementById("diceImg");
+
+       if (d.src.match("static1")){
+
+        f.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyAH5jxs6I2fR_agC0lp5aK9CBCgM4vN_TQ&q=cafes+near+me";
+
+       }
+
+       if (d.src.match("static2")){
+
+        f.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyAH5jxs6I2fR_agC0lp5aK9CBCgM4vN_TQ&q=movies+near+me";
+
+       }
+
+       if (d.src.match("static3")){
+
+        f.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyAH5jxs6I2fR_agC0lp5aK9CBCgM4vN_TQ&q=kayaking+near+me";
+
+       }
+
+       if (d.src.match("static4")){
+
+        f.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyAH5jxs6I2fR_agC0lp5aK9CBCgM4vN_TQ&q=museums+near+me";
+
+       }
+
+       if (d.src.match("static5")){
+
+        f.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyAH5jxs6I2fR_agC0lp5aK9CBCgM4vN_TQ&q=arcades+near+me";
+
+       }
+
+       if (d.src.match("static6")){
+
+        f.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyAH5jxs6I2fR_agC0lp5aK9CBCgM4vN_TQ&q=shopping+near+me";
+
+       }
+
+
      }
 
 
